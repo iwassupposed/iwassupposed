113b3211dd6eed564714959cb2956c1ed2360be1
iwassupposed <matteogari97@gmail.com>
2025-06-09 19:48:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let allPhrases = [];
 
 function getRandomPhrase() {
+  if (allPhrases.length === 0) return;
   const randomPhrase = allPhrases[Math.floor(Math.random() * allPhrases.length)];
   document.getElementById("phrase").innerText = randomPhrase;
 }
 
-fetch('./phrases.json')
-  .then(response => response.json())
-  .then(phrases => {
-    allPhrases = Object.values(phrases);
-    getRandomPhrase();
-  });
-
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('./phrases.json')
+    .then(response => response.json())
+    .then(phrases => {
+      allPhrases = Object.values(phrases);
+      getRandomPhrase();
+    });
+
   const button = document.getElementById("newPhrase");
   if (button) {
     button.addEventListener("click", getRandomPhrase);
